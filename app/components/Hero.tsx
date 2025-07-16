0a8c9287249cb266ec7043d0673cdaab77257b6e
Kalaiarasan Ayyanar <kalaiarasan.ayyanar@gmail.com>
2025-07-16 16:18:24 +0530

    feat: converted and compressed assets to web friendly formats and sizes respectively

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/7670835-uhd_3840_2160_30fps.mp4" type="video/mp4" />
+        <source src="/7670835-uhd_3840_2160_30fps.webm" type="video/webm" />
       </video>
 
       {/* Overlay */}
